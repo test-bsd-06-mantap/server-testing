3c26d88d868e0eb2631163ee03f7732ec63469aa
Joshua Christian <jochris1506@gmail.com>
2023-09-26 10:00:18 +0700

    feat:login

--- app.js
+++ app.js
@@ -6,6 +6,16 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
+//INI KERJAAN NICO, LOGIN BANG
+app.post('/login', async (req, res) => {
+    try {
+        const login = "login sendiri lah"
+        res.status(200).json(login)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
