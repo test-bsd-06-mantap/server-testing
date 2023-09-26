5c32e31107fa429a4c2721f618800e7dc7090903
Joshua Christian <jochris1506@gmail.com>
2023-09-26 10:18:33 +0700

    feat:bang** lu manager

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.post('/login', async (req, res) => {
     } catch (error) {
         res.status(500).json(error)
     }
-}
+})
 
 // INI KERJAAN AXEL, REGISTER
 // udah kelar, tolong anggep ini kerjaan saya
@@ -22,6 +22,16 @@ app.post('/register', async (req, res) => {
     try {
         const register = "regis sendiri lah"
         res.status(201).json(register)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
+
+// get fav, ini kerjaan saya bang
+app.get('/wishlist', async (req, res) => {
+    try {
+        const wishlist = "wishlist sendiri lah"
+        res.status(200).json(wishlist)
     } catch (error) {
         res.status(500).json(error)
     }
