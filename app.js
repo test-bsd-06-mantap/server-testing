58f48b2418cad1281d8ad0b39bb86787f329799f
Joshua Christian <jochris1506@gmail.com>
2023-09-26 09:52:19 +0700

    feat:register:done

--- app.js
+++ app.js
@@ -6,6 +6,17 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
+// INI KERJAAN AXEL, REGISTER
+// udah kelar, tolong anggep ini kerjaan saya
+app.post('/register', async (req, res) => {
+    try {
+        const register = "regis sendiri lah"
+        res.status(201).json(register)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
